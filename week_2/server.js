339940a7cf2621a0162c63473b31c3a0084a92db
jebrsan thatcroos <jebarsanthatcroos@gmail.com>
2026-07-17 03:15:43 +0530

    Merge remote and local repositories week 3

--- week_2/server.js
+++ week_2/server.js
@@ -5,11 +5,9 @@ dotenv.config({ path: path.join(__dirname, "./config.env") });
 
 const app = require("./app");
 
-<<<<<<< HEAD
+
 const PORT = process.env.PORT || 4000;
-=======
-const PORT = process.env.PORT || 5000;
->>>>>>> 76e9bd0738bcbd5b764ef6512064dee02b0904be
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT} in ${process.env.NODE_ENV || "development"} mode`);
